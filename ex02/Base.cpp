c45983908f6c939e84dc35e52b9ddc14851c8873
Chiara Kappe <chiara@Chiaras-MacBook-Pro.local>
2026-06-20 20:19:21 +0200

    changed the random generate from using if/else to using switch/case

--- ex02/Base.cpp
+++ ex02/Base.cpp
@@ -6,25 +6,26 @@
 /*   By: ckappe <ckappe@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/03 17:27:14 by ckappe            #+#    #+#             */
-/*   Updated: 2026/04/03 17:32:32 by ckappe           ###   ########.fr       */
+/*   Updated: 2026/06/20 20:16:50 by ckappe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
 
-Base* generate(void)
-{
-    int random = rand() % 3;
-    if (random == 0)
-        return new A();
-    else if (random == 1)
-        return new B();
-    else
-        return new C();
+Base* generate(void) {
+  std::srand(static_cast<unsigned int>(std::time(0)));
+  int r = std::rand() % 3;
+  switch (r) {
+  case 0:
+    return new A;
+  case 1:
+    return new B;
+  default:
+    return new C;
+  }
 }
 
-void identify(Base* p)
-{
+void identify(Base* p){
     // Pointer dynamic_cast returns NULL on mismatch, so we can chain checks
     if (dynamic_cast<A*>(p))
         std::cout << "A\n";
@@ -36,8 +37,7 @@ void identify(Base* p)
         std::cout << "Unknown type\n";
 }
 
-void identify(Base& p)
-{
+void identify(Base& p){
     try
     {
         // Reference dynamic_cast throws std::bad_cast on mismatch
